04d86cbc6d609567a04bdf13309cd15fac3dd98d
elkamill0 <58825229+elkamill0@users.noreply.github.com>
2024-02-19 10:48:03 +0100

    Add files via upload

---
+++ check_plans.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://wmii.uwm.edu.pl/~mirek/PLANY/'
+
+def check():
+    changes = []
+    response = requests.get(url)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'html.parser')
+    text = soup.get_text().split('\n')
+    text = [x for x in text if x != '']
+
+    links = []
+    for a in soup.find_all('a', href=True):
+        links.append(url + a['href'])
+    links.pop(2)
+    with open("data.txt", "r", encoding='utf-8') as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            line = lines[i].strip().split('\t')
+            if line[1] != text[i].strip():
+                if line[1][:4] == "I-io":
+                    changes.append("I-io: " + links[i])
+                elif line[1][:5] == "I-isi":
+                    changes.append("I-isi: " + links[i])
+                elif line[1][:5] == "II-io":
+                    changes.append("II-io: " + links[i])
+                elif line[1][:6] == "II-isi":
+                    changes.append("II-isi: " + links[i])
+                elif line[1][:6] == "III-io":
+                    changes.append("III-io: " + links[i])
+                elif line[1][:7] == "III-isi":
+                    changes.append("III-isi: " + links[i])
+    if(changes):
+        with open("data.txt", "w", encoding='utf-8') as f:
+            for i in range(len(links)):
+                f.write(links[i] + '\t' + text[i] + '\n')
+
+    return changes
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import os.path
 
 from google.auth.transport.requests import Request
@@ -8,21 +9,31 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+import check_plans
+
+
+with open('json/ids.json') as f:
+    json_data = json.load(f)
+    calendarId = json_data['calendarId']
+    bot_token = json_data['discord_bot_token']
+    discord_bot_channel_id = json_data['discord_bot_channel_id']
+
+
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 creds = None
-if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+if os.path.exists("json/token.json"):
+    creds = Credentials.from_authorized_user_file("json/token.json", SCOPES)
 if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
-      "credentials.json", SCOPES
+      "json/credentials.json", SCOPES
         )
         creds = flow.run_local_server(port=0)
-    with open("token.json", "w") as token:
+    with open("json/token.json", "w") as token:
         token.write(creds.to_json())
 
 service = build('calendar', 'v3', credentials=creds)
@@ -30,7 +41,28 @@ service = build('calendar', 'v3', credentials=creds)
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
-calendarId = 'not for dog'
+
+@bot.event
+async def on_ready():
+    print("Done!")
+    send_msg.start()
+
+
+@bot.command()
+async def poll(ctx, *s):
+    emotes = ['🟢', '🔴', '🟡', '🔵', '🟠', '🟣', '⚪', '⚫']
+
+    s = " ".join(s)
+    s = s.split(":")
+    q = s[0]
+    a = s[1].split(",")
+
+    mess = "# **" + q + "**" + '\n'
+    for i, e in zip(a, emotes):
+        mess += e + " " + i + '\n'
+    message = await ctx.send(mess)
+    for e in range(len(a)):
+        await message.add_reaction(emotes[e])
 
 @bot.event
 async def on_scheduled_event_create(event):
@@ -63,7 +95,7 @@ async def on_scheduled_event_delete(event):
     event = service.events().delete(calendarId=calendarId, eventId = event.id).execute()
 
 @bot.event
-async def on_scheduled_event_update(before, after):
+async def on_scheduled_event_update(after):
     event = service.events().get(calendarId = calendarId, eventId = after.id).execute()
     end_time = after.end_time.strftime('%Y-%m-%dT%H:%M:%S+00:00')
     start_time = after.start_time.strftime('%Y-%m-%dT%H:%M:%S+00:00')
@@ -76,6 +108,13 @@ async def on_scheduled_event_update(before, after):
 
     event = service.events().update(calendarId=calendarId, eventId=after.id, body=event).execute()
 
+@tasks.loop(minutes=30.0)
+async def send_msg():
+    messages = check_plans.check()
+    channel = bot.get_channel(discord_bot_channel_id)
+    print(messages)
+    if messages:
+        for mess in messages:
+            await channel.send(mess)
 
-
-bot.run('not for dog')
+bot.run(bot_token)
